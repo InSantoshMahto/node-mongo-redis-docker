2cf240800cac8e50613f242da81ec9535a81d0cb
Santosh Mahto <smahto@vertel.com.au>
2021-09-02 16:59:24 +0530

    hooks added

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-mongo-redis-docker
+
+sample code
---
+++ test/test.js
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha"
+import { strictEqual } from "assert"
+
+describe("Sample test", () => {
+  it("should return true", function () {
+    strictEqual(true, true)
+  })
+})
